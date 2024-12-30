549cb97c82a110bfc1fd96675879ff9223d4f664
Akaytab Karmakar <akaytab.karmakar@ing.com>
2024-12-30 16:07:55 +0530

    all routes added

--- src/app.js
+++ src/app.js
@@ -29,9 +29,25 @@ app.use(cookieParser());
 // routes import
 import userRoute from './routes/user.routes.js'
 import playlistRoute from './routes/playlist.routes.js';
+// import tweetRouter from "./routes/tweet.routes.js"
+// import subscriptionRouter from "./routes/subscription.routes.js"
+import videoRoute from "./routes/video.routes.js"
+import commentRoute from "./routes/comment.routes.js"
+import likeRouter from "./routes/like.routes.js"
+import playlistRouter from "./routes/playlist.routes.js"
+// import dashboardRouter from "./routes/dashboard.routes.js"
 
 //routes declaration
 app.use('/api/v1/users',userRoute);
-app.use('/api/v1/playlist',playlistRoute)
+app.use('/api/v1/playlist',playlistRoute);
+// app.use("/api/v1/healthcheck", healthcheckRouter)
+// app.use("/api/v1/tweets", tweetRouter)
+// app.use("/api/v1/subscriptions", subscriptionRouter)
+app.use("/api/v1/videos",videoRoute );
+app.use("/api/v1/comments",commentRoute);
+app.use("/api/v1/likes", likeRouter);
+app.use("/api/v1/playlist", playlistRouter);
+// app.use("/api/v1/dashboard", dashboardRouter)
+
 
 export {app};
--- src/routes/comment.routes.js
+++ src/routes/comment.routes.js
@@ -10,3 +10,5 @@ router.use(verifyJwt); // Apply verifyJWT middleware to all routes in this file
 router.route("/:videoId").get(getVideoComments).post(addComment);
 router.route("/c/:commentId/:videoId").delete(deleteComment);
 router.route("/c/:commentId").patch(updateComment);
+
+export default router
